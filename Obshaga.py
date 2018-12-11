91f56dc75e9061a34b9b9924d13326d783c00cca
DariaGugnina <43442239+DariaGugnina@users.noreply.github.com>
2018-12-11 10:05:59 +0300

    Add files via upload

--- Obshaga.py
+++ Obshaga.py
@@ -30,33 +30,19 @@ def search():
 def helping():
   return render_template('help.html')
 
-@app.route('/help_outcome')
-def help_outcome():
-  return "your response sent :)"
-
 @app.route('/request/<requestname>')
 def show_user_request(requestname):
   requestname= db.get_request(requestname)
   return render_template('show_user_request.html', requestname=requestname)
 
-
-@app.route('/you_took_help')
-def you_took_help():
-  return "you_took_help :)"
-
 @app.route('/add_request')
 def add_request():
   return render_template('addrequest.html')
-
-@app.route('/you_publish_request')
-def publish_request():
-  return "Congratulations! You publish request! :)"
 
 @app.route('/user/<username>')
 def userpage(username):
   username = db2.get_user(username),
   return render_template('userpage.html', username = username)
-
 
 
 
@@ -94,43 +80,3 @@ def search1():
     return render_template('page1.html', users=users)
 
 app.run()
-
-
-#4th December
-
-@app.route('/add_user', methods=['GET', 'POST'])
-def add_user():
-    @app.route('/add_user', methods=['GET', 'POST'])
-    def add_user():
-        if request.method == 'POST':
-            # add new user data
-            user = {}
-            user['login'] = request.form.get('login')
-            user['name'] = request.form.get('name')
-            user['course'] = request.form.get('course')
-            user['workplace'] = request.form.get('workplace')
-            user['photo'] = request.form.get('photo')
-
-            # save to database
-            conn = sqlite3.connect('app.db')
-            c = conn.cursor()
-            c.execute("INSERT INTO users "
-                      "(login, name, workplace, course, photo) "
-                      "VALUES "
-                      "('{login}','{name}','{workplace}','{course}','{photo}')"
-                      "".format(**user))
-            conn.commit()
-            conn.close()
-            # redirect to user page
-            return redirect('/user/%s/' % user['login'])
-
-        return render_template("add_user.html")
-
-    @app.route('/search')
-    def search_for_person():
-        q = request.args.get('query')
-        users = db.get_users_by_name(q)
-        return render_template('search_results.html', q=q, users=users)
-
-    app.run()
-
